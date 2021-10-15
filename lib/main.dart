2f5d91542ccea54f04a3f059d2be13bb535e0c3e
manassur <ologundemanasseh@gmail.com>
2021-10-15 15:50:48 +0100

    added font files

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class MyApp extends StatelessWidget {
             // or simply save your changes to "hot reload" in a Flutter IDE).
             // Notice that the counter didn't reset back to zero; the application
             // is not restarted.
+            fontFamily: 'Proxima',
             appBarTheme: AppBarTheme(
               brightness: Brightness.light,
               backwardsCompatibility: false, // 1
